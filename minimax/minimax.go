b1de454b861be3fdd85f92bf7a2701b7326f96ec
Эльнар Нуруллин <elnar@MacBook-Air-Elnar.local>
2021-11-22 09:58:02 +0300

    boarder in depth with comment

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -21,9 +21,10 @@ func minimax(state State, alpha, beta float64, isMaximizer bool, depth uint) (fl
 		return state.Eval() / float64(depth), state
 	}
 
-	// if depth+1 > 8 {
-	// 	return 0, children[0]
-	// }
+	// 11 sec time for first move
+	if depth+1 > 8 {
+		return 0, children[0]
+	}
 
 	var bestState State
 	var bestVal float64
